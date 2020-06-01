d96f7ae787af6756a2a181c013bbaa61952361eb
Joshua Pan <joshuapan8@gmail.com>
2020-06-01 00:57:36 -0700

    Using map and controlled inputs/components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import CardEditor from './CardEditor';
 
-function App() {
-  return <div className="App">Testing</div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: [
+        { front: 'front1', back: 'back1' },
+        { front: 'front2', back: 'back2' },
+      ],
+    };
+  }
+
+  render() {
+    return <CardEditor cards={this.state.cards} />;
+  }
 }
 
 export default App;
---
+++ src/CardEditor.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './CardEditor.css';
+
+class CardEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { front: '', back: '' };
+  }
+
+  handleChange = event =>
+    this.setState({ [event.target.name]: event.target.value });
+
+  render() {
+    const cards = this.props.cards.map((card, index) => {
+      return (
+        <tr key={index}>
+          <td>{card.front}</td>
+          <td>{card.back}</td>
+          <td>
+            <button>Delete card</button>
+          </td>
+        </tr>
+      );
+    });
+
+    return (
+      <div>
+        <h2>Card Editor</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Front</th>
+              <th>Back</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>{cards}</tbody>
+        </table>
+        <br />
+        <input
+          name="front"
+          onChange={this.handleChange}
+          placeholder="Front of card"
+          value={this.state.front}
+        />
+        <input
+          name="back"
+          onChange={this.handleChange}
+          placeholder="Back of card"
+          value={this.state.back}
+        />
+        <button>Add card</button>
+      </div>
+    );
+  }
+}
+
+export default CardEditor;
