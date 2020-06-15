f789d317c44234b4586f16cfea5cc0c6ba9a6210
Joshua Pan <joshuapan8@gmail.com>
2020-06-14 21:06:32 -0700

    Register React application in Firebase

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ import App from './App';
 
 import { BrowserRouter } from 'react-router-dom';
 
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAC0qaud5YwkNZV7ADHcwf9rpoGH5TPqEs',
+  authDomain: 'bootcamp-ce748.firebaseapp.com',
+  databaseURL: 'https://bootcamp-ce748.firebaseio.com',
+  projectId: 'bootcamp-ce748',
+  storageBucket: 'bootcamp-ce748.appspot.com',
+  messagingSenderId: '782124421793',
+  appId: '1:782124421793:web:b26b23317989a449dad6e0',
+};
+
+firebase.initializeApp(firebaseConfig);
+
 ReactDOM.render(
   <BrowserRouter>
     <App />
