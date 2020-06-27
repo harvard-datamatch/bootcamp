58bbfefde79dfe9f44dbdf85b22c13a8dbc87525
Joshua Pan <joshuapan8@gmail.com>
2020-06-26 22:02:00 -0700

    Implement Homepage with flashcard deck links

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { firebaseConnect, isLoaded } from 'react-redux-firebase';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
 
-const Homepage = () => (
-  <div>
-    <h2>Homepage</h2>
-    <Link to="/editor">Card Editor</Link>
-    <br />
-    <Link to="/viewer">Card Viewer</Link>
-  </div>
-);
+const Homepage = props => {
+  if (!isLoaded(props.homepage)) {
+    return <div>Loading...</div>;
+  }
 
-export default Homepage;
+  const decks = Object.keys(props.homepage).map(deckId => {
+    return (
+      <div key={deckId}>
+        <Link to={`/viewer/${deckId}`}>{props.homepage[deckId].name}</Link>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <h2>Homepage</h2>
+      <Link to="/editor">Create a new flashcards deck!</Link>
+      <h3>Flashcards</h3>
+      {decks}
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  return { homepage: state.firebase.data.homepage };
+};
+
+export default compose(
+  firebaseConnect(['/homepage']),
+  connect(mapStateToProps),
+)(Homepage);
